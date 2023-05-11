6c45e3d63925fec549ec1dcf49303b80857ccc7a
yusufhadi <hadiyusuf889@gmail.com>
2023-05-11 20:29:53 +0800

    slicing sign in

--- resources/js/Components/Input.js
+++ resources/js/Components/Input.js
@@ -4,11 +4,15 @@ export default function Input({
     type = 'text',
     name,
     value,
+    defaultValue,
     className,
+    variant = "primary",
     autoComplete,
     required,
     isFocused,
     handleChange,
+    placeholder,
+    isError,
 }) {
     const input = useRef();
 
@@ -24,14 +28,12 @@ export default function Input({
                 type={type}
                 name={name}
                 value={value}
-                className={
-                    `border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm ` +
-                    className
-                }
+                className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full input-${variant} ${className}`}
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
                 onChange={(e) => handleChange(e)}
+                placeholder={placeholder}
             />
         </div>
     );
