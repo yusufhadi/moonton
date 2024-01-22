6f527e42809ce50764e04608c3976c67d9e92928
yusufhadi <hadiyusuf889@gmail.com>
2024-01-23 01:06:29 +0800

    feat: slicing detail movie

--- resources/js/Components/MovieCard.js
+++ resources/js/Components/MovieCard.js
@@ -1,4 +1,5 @@
 import PropType from "prop-types";
+import { Link } from "@inertiajs/inertia-react";
 
 MovieCard.propType = {
     slug: PropType.string.isRequired,
@@ -29,7 +30,10 @@ export default function MovieCard({ slug, name, category, thumbnail }) {
             >
                 <img src="/icons/ic_play.svg" className="" width="50" alt="" />
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link
+                href={route("prototype.movie.show", slug)}
+                className="inset-0 absolute z-50"
+            ></Link>
         </div>
     );
 }
